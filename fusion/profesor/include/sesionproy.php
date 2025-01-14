b884cb8ef0e3d62e6223ac072bf731c6d5ee50dc
JGYR1806 <jysaccis@gmail.com>
2025-01-13 23:16:49 -0400

    Delete fusion directory

--- fusion/profesor/include/sesionproy.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION) || $_SESSION['tipo']!='proy')
-        header("location: ../index.php");
-
-?>
--- fusion/proyectoViejo/modelo/conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conexion= new mysqli ("localhost", "root", "", "proyecto_bdd");
-$conexion-> set_charset("utf8");
-
-?>
